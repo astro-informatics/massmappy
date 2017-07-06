753be30add1308d21ba8e66d50e88debb97f6195
Christopher Wallis <cwallis@mssl7n.mssl.ucl.ac.uk>
2017-07-06 10:56:53 +0100

    started lsst challenge 1

---
+++ src/python/cy_lsst_challenge_1_utils.pyx
@@ -0,0 +1,5 @@
+import numpy as np
+cimport numpy as np
+import healpy as hp
+import pyssht as ssht
+
---
+++ src/python/lsst_challenge_1.py
@@ -0,0 +1,16 @@
+#importing packages                                                                                                                                
+import scipy.io as sio
+import numpy as np
+import scipy as sp
+import scipy.ndimage as im
+import random as rn
+import healpy as hp
+import matplotlib.mlab as mlab
+import pyssht as ssht
+import cy_mass_mapping as mm
+import cy_healpy_mass_mapping as hp_mm
+import cy_DES_utils as DES
+from matplotlib import pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from pylab import cm
+
